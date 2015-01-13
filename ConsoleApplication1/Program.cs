cf4d101370abbb38cb2096da1a2733ebad910d80
Diana Nikolova <diana_nikolova93@abv.bg>
2015-01-13 16:49:21 +0200

    add problem

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yourAge;
+            Console.WriteLine("Enter your age:");
+            Console.WriteLine();
+            yourAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age after ten years is : " + (yourAge + 10));
+        }
+    }
+}
